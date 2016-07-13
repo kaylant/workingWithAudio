369685ee0eb4d2f66b6f99312fd0be3c097d001a
kaylant <kaylant@gmail.com>
2016-07-12 23:04:10 -0500

    audio connected

--- workingWithAudio/ViewController.swift
+++ workingWithAudio/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
+// audio visual foundation framework
+import AVFoundation
+
 class ViewController: UIViewController {
+    
+    // this will act as a controller for our music
+    var player: AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set path for player
+        let audioPath = NSBundle.mainBundle().pathForResource("busoni-indiandiary-3-bertoli", ofType: "mp3")!
+        
+        do {
+        
+            // inserted path into player
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+            
+            // will only attempt to make player play, if try line above works
+            player.play()
+            
+        // if we cannot run player, do something else
+        } catch {
+        
+            // Process error here
+        
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
